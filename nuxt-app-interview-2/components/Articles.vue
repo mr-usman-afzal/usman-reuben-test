c4b2b87b40ea1e0afa04abc6f88ff8cf5e52c15e
Usman Afzal <mohammad.usman.afzal10@gmail.com>
2025-02-06 16:57:41 +0500

    add optimization and image support

---
+++ nuxt-app-interview-2/components/Articles.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="grid">
+    <Article
+      :article="article"
+      v-for="article of articles"
+      :key="article.title"
+    />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  articles: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.grid {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.grid > * {
+  width: calc(100% / 3);
+}
+</style>
